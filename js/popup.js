a4380f0144c28c4c57974a2683271571488e307a
Benoit <benoit.pierrehumbert.pro@gmail.com>
2022-09-05 14:14:59 +0200

    error with corb

--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,35 @@
 // Initialize button with user's preferred color
 let changeColor = document.getElementById("changeColor");
+function addCss(fileName) {
 
+
+}
+// The body of this function will be executed as a content script inside the
+// current page
+function setPageBackgroundColor() {
+
+
+}
+
+function doProcess (){
+    element = document.getElementsByClassName("content-with-sidemenu content-without-sidebar")[0];
+
+    element.style.animation = "10s linear rainbow-bg";
+    element.style.animationIterationCount = "infinite";
+    //element.style.backgroundColor = "red";
+    document.body.appendChild(element);
+   
+
+
+    var head = document.head;
+    var link = document.createElement("link");
+
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    link.href = "https://raw.githubusercontent.com/BPTdev/RAINBOW-INTRANET/main/css/rainbow-smoothed.css";
+
+    head.appendChild(link);
+}
 
 // When the button is clicked, inject setPageBackgroundColor into current page
 changeColor.addEventListener("click", async () => {
@@ -8,35 +37,6 @@ changeColor.addEventListener("click", async () => {
 
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
-        func: setPageBackgroundColor,
+        func: doProcess,
     });
 });
-
-
-function addStyleInHtml () {
-    document.head.insertAdjacentHTML("beforeend", `<style></style>`)
-}
-
-// The body of this function will be executed as a content script inside the
-// current page
-function setPageBackgroundColor() {
-    element = document.getElementsByClassName("intranet-inner")[0];
-
-    element.style.animation = "10s linear rainbow-bg";
-    element.style.animationIterationCount = "infinite";
-    //element.style.backgroundColor = "red";
-    document.body.appendChild(element);
-}
-function addCss(fileName) {
-
-    var head = document.head;
-    var link = document.createElement("link");
-
-    link.type = "text/css";
-    link.rel = "stylesheet";
-    link.href = fileName;
-
-    head.appendChild(link);
-}
-
-addCss('{my-url}');
