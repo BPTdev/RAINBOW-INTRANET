c917f7d491c7111215697b27435c2303a01f25ab
Benoit <47849605+BPTdev@users.noreply.github.com>
2022-10-05 11:23:21 +0200

    wip multiple classes

--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,9 @@
 // Initialize button with user's preferred color
 let changeColor = document.getElementById("changeColor");
-
+// Initialize variables
+let classes = ["fc-event-bg", "fc-event-head"]
+let url = "https://intranet.cpnv.ch/etudiants/benoit_pierrehumbert/agenda"
+let timeToWait = 4000
 function doProcess (){
     style = ".rainbow-bg{\n" +
         "    animation: rainbow-bg 10s linear;\n" +
@@ -44,35 +47,51 @@ function doProcess (){
         "        background-color: rgb(255,0,127, 0.30);\n" +
         "    }\n" +
         "}";
-    elements = document.getElementsByClassName("fc-event-bg");
-
     var head = document.head;
     var link = document.createElement("style");
 
     link.textContent = style;
-
     head.appendChild(link);
+
+
+
+
+
+    //cunt the number of lines in the array to determine what do with the content or the classes array
+    if (classes.count===1)
+    {
+        elements = document.getElementsByClassName("fc-event-bg");
+    }else
+    {
+        //for each element in the array classes, get his element by class name
+        Array.from(classes).forEach(oneClass => {
+            console.log(oneClass)
+            elements.push(document.getElementsByClassName(oneClass))
+        });
+
+    }
+
+
+//for each element in the array elements, put the rainbow-bg class name in his dom element
     Array.from(elements).forEach(element => {
         console.log(element)
         element.classList.add('rainbow-bg');
 
     });
 
-    elements2 = document.getElementsByClassName("fc-event-head");
 
-    Array.from(elements2).forEach(element2 => {
-        console.log(element2)
-        element2.classList.add('rainbow-bg');
-
-    });
 }
-
+function waitForPageLoad() {
+    alert('fasfas')
+    window.open(url, "_blank");
+    setTimeout(doProcess,4000);
+}
 // When the button is clicked, inject setPageBackgroundColor into current page
 changeColor.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
-        func: doProcess,
+        func: waitForPageLoad,
     });
 });
