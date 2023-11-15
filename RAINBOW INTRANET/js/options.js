1552cbbb2aa9b9515813a9fb11993315cacad6f0
Benoit <47849605+BPTdev@users.noreply.github.com>
2023-11-15 09:28:44 +0100

    Work everywhere

--- RAINBOW INTRANET/js/options.js
+++ RAINBOW INTRANET/js/options.js
@@ -40,6 +40,12 @@ window.onload = function() {
     chrome.storage.sync.get(['url', 'title'], function(data) {
         if (data.url) {
             document.getElementById('urlInput').value = data.url;
+            document.getElementById('urlDisplay').textContent = data.url;
+            document.getElementById('titleDisplay').textContent = data.title;
+        }
+        else {
+            document.getElementById('urlDisplay').textContent = 'No URL stored';
+            document.getElementById('titelDisplay').textContent = 'No Title stored';
         }
         if (data.title) {
             document.getElementById('titleInput').value = data.title;
@@ -54,6 +60,14 @@ document.getElementById('saveButton').addEventListener('click', function() {
     chrome.storage.sync.set({ url, title }, function() {
         console.log('URL and Title saved');
     });
+});
+// Charger l'URL enregistrée
+chrome.storage.sync.get(['url'], function(data) {
+    if (data.url) {
+        document.getElementById('urlDisplay').textContent = data.url;
+    } else {
+        document.getElementById('urlDisplay').textContent = 'No URL stored';
+    }
 });
 
 
